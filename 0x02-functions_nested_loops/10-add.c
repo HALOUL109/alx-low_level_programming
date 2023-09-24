9d301afc59296a014e7ad428af0bbbb35e93b62d
Ahmed Haloul <133600102+HALOUL109@users.noreply.github.com>
2023-09-25 02:33:32 +0300

    Add files via upload

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: add two integers
+ * @a: The first number to be computed
+ * @b: The second number to be computed
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry point
+ *
+ * Description: Prints all natural numbers from n to 98
+ * @n: The input number
+ *
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
